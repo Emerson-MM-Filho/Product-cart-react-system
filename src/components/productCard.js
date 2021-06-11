4525ea09ef6cc138ddf67e22490134106f0490fa
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-11 16:01:12 -0400

    make changes to transform product card in an dynamic component

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -1,21 +1,22 @@
 import './productCard.scss'
-import image from './teste.jpg'
 
-const ProductCard = () => {
-  const url = ''
+const ProductCard = (props) => {
+  const { id, title, price, image } = props.product
   return (
-    <div className="card" >
-      <img src={image} className="card-img-top" alt=''/>
-        <div className="card-body">
-          <h5 className="card-title">Product name</h5>
-          <div className="card-price-container">
-            <h5 className="card-current-price">R$ 99,99</h5>
-            <h5 className="card-last-price">R$ 159,99</h5>
-          </div>
-          <a className="btn btn-primary" href={url}>
-            Add to cart
-          </a>
+    <div className="card" id={id}>
+      <div className="card-body product-card">
+        <img src={image} className="card-img-top" alt=''/>
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <div className="card-price-container">
+          <h5 className="card-current-price">R$ {price.toFixed(2)}</h5>
+          <h5 className="card-last-price">R$ {(price * 1.1).toFixed(2)}</h5>
         </div>
+        <button className="btn btn-primary">
+          Add to cart
+        </button>
+      </div>
     </div>
   )
 }
