4dc1674c3da7a8ecf198aaf4391ab3de54c2ca7a
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-11 16:01:34 -0400

    get all products from api and render in page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 import './App.scss';
 import ProductCard from "./components/productCard";
 
 function App() {
+  const [ allProducts, setAllProducts] = useState([])
+  fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(arrayProducts => setAllProducts(arrayProducts))
+
   return (
     <div className='page-content'>
-      <ProductCard/>
+      <div className='all-products-cards'>
+        {allProducts.map((current, index) => <ProductCard product={current} key={index}/>)}
+      </div>
+      <div className='product-cart-container'>
+
+      </div>
     </div>
   );
 }
