0ac88f13ff2f7c07562477cbc69f668dd49d84e4
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-12 19:55:00 -0400

    add total price

--- src/components/productCart.js
+++ src/components/productCart.js
@@ -2,6 +2,11 @@ import './productCart.scss'
 import ProductInCart from "./productInCart";
 
 const ProductCart = (props) => {
+  const totalPrice = props.productsToAdd.reduce((acc, current) => {
+    acc += current.price
+    return acc
+  }, 0)
+
   return (
     <div className='cart'>
       <h2>My products cart</h2>
@@ -10,6 +15,13 @@ const ProductCart = (props) => {
         <h3>Price</h3>
       </div>
       {props.productsToAdd.map((current, index) => <ProductInCart product={current} key={index}/>)}
+      <div className='total-price'>
+        <h3>Total:
+          <span>
+            ${totalPrice.toFixed(2)}
+          </span>
+        </h3>
+      </div>
     </div>
   )
 }
